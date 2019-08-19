399b0ffdd7bccf405609309739669d1660ba1c29
StanisciaW <53986063+StanisciaW@users.noreply.github.com>
2019-08-19 11:27:59 -0400

    Add files via upload

---
+++ Decisions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Console.WriteLine("Bob's Big Giveaway");
+            //Console.WriteLine("Choose a door: 1, 2 or 3: ");
+            //string userValue = Console.ReadLine();
+
+            //string message = "";
+
+            //if (userValue == "1")
+            
+            //    message = "You won a new car!";
+               
+            
+            //else if(userValue == "2")
+            
+                 //message = "you won a new boat!";
+                
+            
+            //else if (userValue == "3")
+            
+                 //message = "you won a new cat?!";
+                
+            
+            //else
+            //{
+            //    message = "sorry, we didnt understand.";
+                //message = message + "You Lose.";
+                //message += "You Lose.";
+                
+            //}
+            //Console.WriteLine(message);
+            //Console.ReadLine();
+            
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.WriteLine("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint ";
+
+            //Console.WriteLine("You won a ");
+            //Console.WriteLine(message);
+            //Console.WriteLine(".");
+
+            //Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperatorsExpressionsStatements.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExpressionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable declaration 
+            int x, y, a, b;
+
+            // Assignment operator
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            // Mathematical operators
+
+
+            // Addition operator
+            x = 3 + 4;
+
+            // subtraction operator
+            x = 4 - 3;
+
+            // Multipication operator
+            x = 10 * 5;
+
+            // Division operator
+            x = 10 / 5;
+
+            // order of operations using parentesis
+            x = (x + y) * (a - b);
+
+
+            // Operators used to evaluate values
+
+            // Equality operator
+            if (x == y)
+            {
+
+            }
+
+            // Greater than operator
+            if (x > y)
+            {
+
+            }
+
+
+            // Less than operator
+            if (x < y)
+            {
+
+            }
+
+            // Greater than equal to operator
+
+        if (x >= y)
+            {
+
+            }
+        if (x <= y)
+            {
+
+            }
+
+        // there are two "conditional" operators as well that can be used to expand / enhance an evaluation and they can be cpmbined together multiple times.
+
+            // Conditional AND operator
+            if ((x > y) && (a > b))
+            {
+
+            }
+
+
+
+            // Conditional OR operator
+            if ((x > y) || (a > b))
+            {
+
+            }
+
+
+            // Also, heres the in line conditional operator 
+            string message = (x == 1) ? "Car" : "Boat";
+
+            // Message access and method invocation
+            Console.WriteLine("Hi");
+
+
+        }
+    }
+}
